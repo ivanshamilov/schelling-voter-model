7cfd255bdc696aa5503bf643bf85a946f4a26bd0
Ivan Shamilov <shamilov.ivan21@gmail.com>
2023-01-26 23:53:38 +0100

    Refactor and simulation core functions

--- logic.py
+++ logic.py
@@ -1,6 +1,29 @@
 import numpy as np
 from numba import njit
 import random
+import matplotlib.pyplot as plt
+
+
+def plot_lattice(lattice: np.array, show_labels: bool = False):
+    """
+    Plot lattice with specified colors
+    """
+    data_3d = np.ndarray(shape=(lattice.shape[0], lattice.shape[1], 3), dtype=int)
+    color_map = {0: np.array([255, 255, 255]), # WHITE
+                1: np.array([0, 0, 0]), # BLACK
+                -1: np.array([255, 0, 0])} # blue 
+    for i in range(0, lattice.shape[0]):
+        for j in range(0, lattice.shape[1]):
+            data_3d[i][j] = color_map[lattice[i][j]]
+    fig, ax = plt.subplots(1,1, figsize=(9, 9))
+    ax.imshow(data_3d)  
+    if show_labels:
+        for i in range(0, lattice.shape[0]):
+            for j in range(0, lattice.shape[1]):
+                c = lattice[j,i]
+                ax.text(i, j, str(c), va='center', ha='center')
+
+    plt.show()
 
 def initialize(L: int, p: float = 1.0) -> np.array:
     '''
@@ -36,49 +59,91 @@ def count_speakers(lattice: np.array, agent_language: int = 1):
            np.count_nonzero(lattice == -agent_language)
 
 @njit
-def find_tolerance(lattice: np.array, i: int, j: int, return_counts: bool=False):
+def fraction_minority_language(lattice: np.array):
+    """
+    Calculate the fraction of the minority language
+    """
+    speakers_1, speakers_2 = count_speakers(lattice)
+    return min(speakers_1, speakers_2) / (speakers_1 + speakers_2)
+
+@njit
+def find_tolerance(lattice: np.array, i: int, j: int, color: int=0, return_counts: bool=False):
     """
     Count all neighbours and neighbours of the other type for agent (i, j)
     """
     neighbours = 0
     other_type = 0
-    
+    if color == 0:
+        color = lattice[i][j]
     for x in [i, i - 1, i + 1]:
         for y in [j, j - 1, j + 1]:
             if (x, y) == (i, j):
                 continue
             curr = lattice[x % lattice.shape[0]][y % lattice.shape[1]]
             neighbours += np.abs(curr)
-            if curr not in [lattice[i][j], 0]:
+            if curr not in [color, 0]:
                 other_type += 1
     if return_counts:
         return (neighbours, other_type)
     return (other_type / neighbours, 0) if neighbours != 0 else (0, 0)
 
+@njit 
+def general_tolerance(lattice: np.array, tolerance: float, threshold: float = 0.99):
+    """
+    Checks if the system is stable (the number of unhappy agents is smaller than the threshold value)
+    """
+    # acceptable number of unhappy agents
+    allowed_number_of_unhappy_agents = int((1 - threshold) * lattice.shape[0] * lattice.shape[1]) 
+    c = 0 # current number of unhappy agents
+    for (i, j), x in np.ndenumerate(lattice):
+        curr_tolerance = find_tolerance(lattice, i, j)[0]
+        if curr_tolerance >= tolerance:
+            c += 1
+        if c >= allowed_number_of_unhappy_agents: 
+            return 0  # the number of unhappy agents crossed the threshold -> system is unstable yet
+    return 1  # system is stable
+
 @njit
 def schelling_migrate(lattice: np.array, i: int, j: int, tolerance: float): 
-    is_happy = False
+    """
+    Finds a place to migrate the agent (i, j) until a happy place was found 
+    or max iterations number reached
+    """
+    num_iters = int(lattice.shape[0] * lattice.shape[1] / 20)
     x, y = 0, 0 # looking for a new home :) 
-    while not is_happy:
-        # repeat until a "happy" spot found
-        empty_spots = find_empty_spots(lattice)
-        x, y = empty_spots[np.random.randint(empty_spots.shape[0])] # choose empty spot randomly
-        lattice[x][y] = lattice[i][j] # test migrate
-        potential_tolerance, _ = find_tolerance(lattice, x, y) # tolerance on the new place
-        if potential_tolerance <= tolerance:
-            is_happy = True
-        else:
-            lattice[x][y] = 0
+    flag = 0
 
-    lattice[i][j] = 0 # move out from the initial place
+    for _ in range(num_iters):
+        empty_spots = find_empty_spots(lattice)
+        if empty_spots.shape[0] == 0:
+            break
+        x, y = empty_spots[np.random.randint(empty_spots.shape[0])] # choose empty spot randomly
+        potential_tol, _ = find_tolerance(lattice, x, y, color=lattice[i][j]) # tolerance on the new place
+        if potential_tol <= tolerance:
+            flag = 1
+            break
+
+    if flag == 1:    # happy place was found -> migrate
+        lattice[x][y] = lattice[i][j]
+        lattice[i][j] = 0 # move out from the initial place
 
     return lattice
 
 @njit
 def voter_new_language(lattice: np.array, i: int, j: int, prestige: float): 
+    """
+    Learning new language depending on the transition probability and the prestige of the language
+    """
     agent_language = lattice[i][j]
     agent_language_count, other_language_count = count_speakers(lattice, agent_language)
-    # TODO
+
+    prob = other_language_count / (2 * (agent_language_count + other_language_count))
+
+    # TODO check prestige
+    if np.random.uniform(0, 1) < prob:
+        lattice[i][j] = -agent_language
+
+    return lattice
 
 @njit
 def update(lattice: np.array, i: int, j: int, tolerance: float, prob_migrate: float, prestige: float):
@@ -91,17 +156,11 @@ def update(lattice: np.array, i: int, j: int, tolerance: float, prob_migrate: fl
     agent_tol, _ = find_tolerance(lattice, i, j)
     if agent_tol > tolerance:
         if np.random.uniform(0, 1) < prob_migrate:
-            # Schelling behaviour (try to migrate). 
-            # Change is not mandatory, migration can fail if the agent is unhappy in 
-            # any emtty space
-            schelling_migrate(lattice, i, j, tolerance)
+            lattice = schelling_migrate(lattice, i, j, tolerance)
         else:
-            # Voter behaviour (try to learn a new language)
-            # The language change would depend on transition probabilities q
-            # which can include both the number of agent of each language 
-            # (locally or globally measured) and the perceived prestige of the other 
-            # language
-            voter_new_language(lattice, i, j, prestige)
+            lattice = voter_new_language(lattice, i, j, prestige)
+
+    return lattice
 
 @njit
 def simulation(lattice: np.array, tolerance: float, prob_migrate: float, prestige: float):
@@ -109,12 +168,21 @@ def simulation(lattice: np.array, tolerance: float, prob_migrate: float, prestig
     Single simulation for the whole lattice
     """
     N = lattice.shape[0]
-    run_for = 100
-    curr_step = 0
-    for _ in range(N * N):
-        if curr_step == run_for:
-            break
+    run_for = 50000
+    
+    initial_values = count_speakers(lattice, 1)
+    # TODO: check time steps
+    for i in range(run_for):
+        if i % 10000 == 0:
+            print(f"Step {i} / {run_for}")
         agents = find_agents(lattice)
         x, y = agents[np.random.randint(agents.shape[0])] # make sure we choose agent (non-zero element)
         update(lattice, x, y, tolerance, prob_migrate, prestige)
-        curr_step += 1
+        if i % 100 == 0:
+            if general_tolerance(lattice, tolerance):
+                break
+    
+    print("Starting values: ", initial_values)
+    print("Ending values: ", count_speakers(lattice, 1))
+
+    return lattice
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from logic import *
 
 '''
@@ -7,6 +8,18 @@ img = plt.imshow(lattice, cmap="plasma")
 plt.xlabel("L [i]")
 plt.ylabel("L [j]")
 plt.show()
+
+
+lattices = np.zeros((10, L, L))
+lattices[i] = lattice
+
+fig, axes = plt.subplots(2, 5, figsize=(10, 10))
+
+for lat, ax in zip(lattices, axes.flatten()):
+  ax.matshow(lat, cmap="plasma")
+plt.show()
+
+
 '''
 
 if __name__ == "__main__":
@@ -20,17 +33,29 @@ if __name__ == "__main__":
   p_density = np.arange(0.02, 0.98, 0.04) # 24 steps
   '''
 
-  # N = 50
-  # p = N / L ** 2
-
-  L = 10  # lattice dimension
-  density = 0.5 # formula = N / L ** 2 -> N (number of agents) can be estimated
+  L = 50  # lattice dimension
+  density = 0.9 # formula = N / L ** 2 -> N (number of agents) can be estimated
   tolerance = 0.3 # tolerance
   prestige = 0.3 # prestige of language
-  prob_migrate = 0.5 # possibility to migrate 
+  prob_migrate = 0.2  # possibility to migrate 
+
+  num_iters = 30
+  probs = np.arange(0, 1, 0.1)
+  simulation_results = np.zeros(probs.shape[0])
+  
+  for i, prob_mig in enumerate(probs):
+    sim_res = 0
+    for j in range(num_iters):
+      print(f"Iteration {j + 1} out of {num_iters}")
+      lattice = initialize(L, density)
+      lattice = simulation(lattice, tolerance, prob_mig, prestige)
+      res = fraction_minority_language(lattice)
+      print(f"<S>={res}")
+      sim_res += res
+
+    simulation_results[i] = sim_res / num_iters
 
 
-  lattice = initialize(L, density)
-  print(lattice)
-  simulation(lattice, tolerance, prob_migrate, prestige)
-  print(lattice)
+  plt.plot(probs, simulation_results)
+  plt.ylim([-0.1, 1.1])
+  plt.show()
